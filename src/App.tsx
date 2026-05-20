14f2e8c2ed7aa07a3c86a22fa8a09e41eaf3e519
GaiaGD <gaia.digregorio@gmail.com>
2026-05-20 09:35:00 -0400

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import Header from './components/Header/index.tsx'
 import Work from './components/Work/index.tsx'
 import data from '../data.json'
+import Footer from './components/Footer/index.tsx'
 
 function App() {
   return (
     <div className='lg:mx-12 font-geist-mono'>
       <Header {...data.header} />
       <Work items={data.work} />
+      <Footer />
     </div>
   )
 }
